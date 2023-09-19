lazear/sage

commit 06be420bf53cb8a4ad69fcb1a88dc327e03ce018

Report search progress (#86)

crates/sage-cloudpath/src/mzml.rs
@@ -63,6 +63,8 @@ pub struct MzMLReader {
     // If set to Some(level) and noise intensities are present in the MzML file,
     // divide intensities at this MS-level by noise to calculate S/N
     signal_to_noise: Option<u8>,
+
+    file_id: usize,
 }
 
 impl MzMLReader {
@@ -74,8 +76,22 @@ impl MzMLReader {
     pub fn with_level_filter(ms_level: u8) -> Self {
         Self {
             ms_level: Some(ms_level),
+            file_id: 0,
             signal_to_noise: None,
         }
+    }
+
+    pub fn with_file_id(file_id: usize) -> Self {
+        Self {
+            ms_level: None,
+            signal_to_noise: None,
+            file_id,
+        }
+    }
+
+    pub fn set_file_id(&mut self, file_id: usize) -> &mut Self {
+        self.file_id = file_id;
+        self
     }
 
     pub fn set_signal_to_noise(&mut self, sn: Option<u8>) -> &mut Self {

crates/sage/src/spectrum.rs
@@ -41,7 +41,6 @@ pub struct SpectrumProcessor {
     pub max_fragment_mz: f32,
     pub min_fragment_mz: f32,
     pub deisotope: bool,
-    pub file_id: usize,
 }
 
 #[derive(Default, Debug, Clone)]
@@ -77,6 +76,7 @@ pub struct ProcessedSpectrum {
 #[derive(Default, Debug, Clone)]
 /// An unprocessed mass spectrum, as returned by a parser
 pub struct RawSpectrum {
+    pub file_id: usize,
     /// MSn level
     pub ms_level: u8,
     /// Spectrum identifier
@@ -240,20 +240,17 @@ impl SpectrumProcessor {
     /// * `min_fragment_mz`: Keep only fragments >= this m/z
     /// * `max_fragment_mz`: Keep only fragments <= this m/z
     /// * `deisotope`: Perform deisotoping & charge state deconvolution
-    /// * `file_id`: Store this value in all [`ProcessedSpectrum`]
     pub fn new(
         take_top_n: usize,
         min_fragment_mz: f32,
         max_fragment_mz: f32,
         deisotope: bool,
-        file_id: usize,
     ) -> Self {
         Self {
             take_top_n,
             min_fragment_mz,
             max_fragment_mz,
             deisotope,
-            file_id,
         }
     }
 
@@ -335,7 +332,7 @@ impl SpectrumProcessor {
         ProcessedSpectrum {
             level: spectrum.ms_level,
             id: spectrum.id,
-            file_id: self.file_id,
+            file_id: spectrum.file_id,
             scan_start_time: spectrum.scan_start_time,
             ion_injection_time: spectrum.ion_injection_time,
             precursors: spectrum.precursors,

crates/sage-types/Cargo.toml
@@ -1,0 +1,8 @@
+[package]
+name = "sage-types"
+version = "0.1.0"
+edition = "2021"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+[dependencies]

crates/sage-cli/src/main.rs
@@ -114,11 +114,30 @@ impl Runner {
         scorer: &Scorer,
         spectra: Vec<ProcessedSpectrum>,
     ) -> SageResults {
+        use std::sync::atomic::{AtomicUsize, Ordering};
+        let counter = AtomicUsize::new(0);
+        let start = Instant::now();
+
         let features: Vec<_> = spectra
             .par_iter()
             .filter(|spec| spec.peaks.len() >= self.parameters.min_peaks && spec.level == 2)
+            .map(|x| {
+                let prev = counter.fetch_add(1, Ordering::Relaxed);
+                if prev > 0 && prev % 10_000 == 0 {
+                    let duration = Instant::now().duration_since(start).as_millis() as usize;
+
+                    let rate = prev * 1000 / (duration + 1);
+                    log::trace!(" - searched {} spectra ({} spectra/s)", prev, rate);
+                }
+                x
+            })
             .flat_map(|spec| scorer.score(spec))
             .collect();
+
+        let duration = Instant::now().duration_since(start).as_millis() as usize;
+        let prev = counter.load(Ordering::Relaxed);
+        let rate = prev * 1000 / (duration + 1);
+        log::info!(" - search:  {:8} ms ({} spectra/s)", duration, rate);
 
         let quant = self
             .parameters
@@ -164,53 +183,35 @@ impl Runner {
             .sn
             .then_some(self.parameters.quant.tmt_settings.level);
 
+        let sp = SpectrumProcessor::new(
+            self.parameters.max_peaks,
+            self.parameters.database.fragment_min_mz,
+            self.parameters.database.fragment_max_mz,
+            self.parameters.deisotope,
+        );
+
         let spectra = chunk
             .par_iter()
-            .map(|path| sage_cloudpath::util::read_mzml(path, sn))
             .enumerate()
-            .filter_map(|(idx, spectra)| match spectra {
-                Ok(spectra) => {
-                    log::trace!(" - {}: read {} spectra", chunk[idx], spectra.len());
-                    Some(spectra)
-                }
-                Err(e) => {
-                    log::error!("error while processing {}: {}", chunk[idx], e);
-                    None
+            .flat_map(|(idx, path)| {
+                match sage_cloudpath::util::read_mzml(path, chunk_idx * batch_size + idx, sn) {
+                    Ok(s) => {
+                        log::trace!("- {}: read {} spectra", path, s.len());
+                        Ok(s)
+                    }
+                    Err(e) => {
+                        log::error!("- {}: {}", path, e);
+                        Err(e)
+                    }
                 }
             })
+            .flat_map_iter(|spectra| spectra.into_iter().map(|s| sp.process(s)))
             .collect::<Vec<_>>();
+
         let io_time = Instant::now() - start;
-
-        let count: usize = spectra.iter().map(|x| x.len()).sum();
-
-        let start = Instant::now();
-        let results = spectra
-            .into_par_iter()
-            .enumerate()
-            .map(|(idx, spectra)| {
-                let sp = SpectrumProcessor::new(
-                    self.parameters.max_peaks,
-                    self.parameters.database.fragment_min_mz,
-                    self.parameters.database.fragment_max_mz,
-                    self.parameters.deisotope,
-                    chunk_idx * batch_size + idx,
-                );
-
-                let spectra = spectra
-                    .into_iter()
-                    .map(move |spec| sp.process(spec))
-                    .collect::<Vec<_>>();
-                self.search_processed_spectra(scorer, spectra)
-            })
-            .collect::<SageResults>();
-        let search_time = Instant::now() - start;
         info!(" - file IO: {:8} ms", io_time.as_millis());
-        info!(
-            " - search:  {:8} ms ({} spectra)",
-            search_time.as_millis(),
-            count
-        );
-        results
+
+        self.search_processed_spectra(scorer, spectra)
     }
 
     pub fn batch_files(&self, scorer: &Scorer, batch_size: usize) -> SageResults {

crates/sage-cli/tests/integration.rs
@@ -12,10 +12,10 @@ fn integration() -> anyhow::Result<()> {
 
     let fasta = sage_cloudpath::util::read_fasta("../../tests/Q99536.fasta", "rev_", true)?;
     let database = builder.make_parameters().build(fasta);
-    let spectra = sage_cloudpath::util::read_mzml("../../tests/LQSRPAAPPAPGPGQLTLR.mzML", None)?;
+    let spectra = sage_cloudpath::util::read_mzml("../../tests/LQSRPAAPPAPGPGQLTLR.mzML", 0, None)?;
     assert_eq!(spectra.len(), 1);
 
-    let sp = SpectrumProcessor::new(100, 0.0, 1500.0, true, 0);
+    let sp = SpectrumProcessor::new(100, 0.0, 1500.0, true);
     let processed = sp.process(spectra[0].clone());
     assert!(processed.peaks.len() <= 300);
 

crates/sage-cloudpath/src/util.rs
@@ -4,10 +4,11 @@ use tokio::io::AsyncReadExt;
 
 pub fn read_mzml<S: AsRef<str>>(
     s: S,
+    file_id: usize,
     signal_to_noise: Option<u8>,
 ) -> Result<Vec<RawSpectrum>, Error> {
     read_and_execute(s, |bf| async move {
-        Ok(crate::mzml::MzMLReader::default()
+        Ok(crate::mzml::MzMLReader::with_file_id(file_id)
             .set_signal_to_noise(signal_to_noise)
             .parse(bf)
             .await?)

crates/sage-types/src/lib.rs
@@ -1,0 +1,14 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}

commit b04bfbe27930a5eb2d80248bf87489f9a3206da0

fix: incorrect filenames in output introduced in 06be420

crates/sage-cli/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "sage-cli"
-version = "0.14.1-alpha"
+version = "0.14.1-alpha.1"
 authors = ["Michael Lazear <michaellazear92@gmail.com"]
 edition = "2021"
 rust-version = "1.62"

crates/sage-cloudpath/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "sage-cloudpath"
-version = "0.14.1-alpha"
+version = "0.14.1-alpha.1"
 authors = ["Michael Lazear <michaellazear92@gmail.com"]
 edition = "2021"
 rust-version = "1.62"

crates/sage-cloudpath/src/mzml.rs
@@ -57,7 +57,6 @@ const SELECTED_ION_CHARGE: &[u8] = b"MS:1000041";
 const ISO_WINDOW_LOWER: &[u8] = b"MS:1000828";
 const ISO_WINDOW_UPPER: &[u8] = b"MS:1000829";
 
-#[derive(Default)]
 pub struct MzMLReader {
     ms_level: Option<u8>,
     // If set to Some(level) and noise intensities are present in the MzML file,
@@ -73,10 +72,10 @@ impl MzMLReader {
     /// # Example
     ///
     /// A minimum level of 2 will not parse or return MS1 scans
-    pub fn with_level_filter(ms_level: u8) -> Self {
+    pub fn with_file_id_and_level_filter(file_id: usize, ms_level: u8) -> Self {
         Self {
             ms_level: Some(ms_level),
-            file_id: 0,
+            file_id,
             signal_to_noise: None,
         }
     }
@@ -115,7 +114,7 @@ impl MzMLReader {
         let mut binary_dtype = Dtype::F64;
         let mut binary_array = None;
 
-        let mut spectrum = RawSpectrum::default();
+        let mut spectrum = RawSpectrum::default_with_file_id(self.file_id);
         let mut precursor = Precursor::default();
         let mut iso_window_lo: Option<f32> = None;
         let mut iso_window_hi: Option<f32> = None;
@@ -194,7 +193,7 @@ impl MzMLReader {
                                 let level = extract_value!(ev);
                                 if let Some(filter) = self.ms_level {
                                     if level != filter {
-                                        spectrum = RawSpectrum::default();
+                                        spectrum = RawSpectrum::default_with_file_id(self.file_id);
                                         state = None;
                                     }
                                 }
@@ -206,7 +205,7 @@ impl MzMLReader {
                                 let value = extract_value!(ev);
                                 if value == 0.0 {
                                     // No ion current, break out of current state
-                                    spectrum = RawSpectrum::default();
+                                    spectrum = RawSpectrum::default_with_file_id(self.file_id);
                                     state = None;
                                 } else {
                                     spectrum.total_ion_current = value;
@@ -357,7 +356,7 @@ impl MzMLReader {
                                 }
                                 (false, _) => {}
                             }
-                            spectrum = RawSpectrum::default();
+                            spectrum = RawSpectrum::default_with_file_id(self.file_id);
                             None
                         }
                         _ => state,
@@ -464,7 +463,7 @@ mod test {
             </binaryDataArrayList>
         </spectrum>
         "#;
-        let mut spectra = MzMLReader::default().parse(s.as_bytes()).await?;
+        let mut spectra = MzMLReader::with_file_id(0).parse(s.as_bytes()).await?;
 
         assert_eq!(spectra.len(), 1);
         let s = spectra.pop().unwrap();

crates/sage/Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "sage-core"
-version = "0.14.1-alpha"
+version = "0.14.1-alpha.1"
 authors = ["Michael Lazear <michaellazear92@gmail.com"]
 edition = "2021"
 rust-version = "1.62"

crates/sage/src/spectrum.rs
@@ -75,6 +75,7 @@ pub struct ProcessedSpectrum {
 
 #[derive(Default, Debug, Clone)]
 /// An unprocessed mass spectrum, as returned by a parser
+/// *CRITICAL*: Users must set all fields manually, including `file_id`
 pub struct RawSpectrum {
     pub file_id: usize,
     /// MSn level
@@ -95,6 +96,17 @@ pub struct RawSpectrum {
     pub mz: Vec<f32>,
     /// Intensity array
     pub intensity: Vec<f32>,
+}
+
+impl RawSpectrum {
+    /// Return a [`RawSpectrum`] with default values, but with the `file_id` field
+    /// properly set
+    pub fn default_with_file_id(file_id: usize) -> Self {
+        Self {
+            file_id,
+            ..Default::default()
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]