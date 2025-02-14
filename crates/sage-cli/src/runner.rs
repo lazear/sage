lazear/carina

feat: use of ion mobility in quant

crates/sage-cli/src/runner.rs
@@ -1,3 +1,4 @@
+
 use super::input::Search;
 use super::output::SageResults;
 use super::telemetry;
@@ -12,7 +13,7 @@ use sage_core::lfq::{Peak, PrecursorId};
 use sage_core::mass::Tolerance;
 use sage_core::scoring::Fragments;
 use sage_core::scoring::{Feature, Scorer};
-use sage_core::spectrum::{ProcessedSpectrum, SpectrumProcessor};
+use sage_core::spectrum::{RawSpectrum, ProcessedSpectrum, SpectrumProcessor,MS1Spectra};
 use sage_core::tmt::TmtQuant;
 use std::collections::HashMap;
 use std::time::Instant;
@@ -21,6 +22,36 @@ pub struct Runner {
     pub database: IndexedDatabase, // I could use a getter if I dont want to make this pub ...
     pub parameters: Search,
     start: Instant,
+}
+
+#[derive(Default)]
+struct RawSpectrumAccumulator {
+    pub ms1: Vec<RawSpectrum>,
+    pub msn: Vec<RawSpectrum>,
+}
+
+impl FromParallelIterator<RawSpectrum> for RawSpectrumAccumulator {
+    fn from_par_iter<I>(par_iter: I) -> Self
+    where
+        I: IntoParallelIterator<Item = RawSpectrum>,
+    {
+        let out = par_iter
+            .into_par_iter()
+            .fold(|| RawSpectrumAccumulator::default(), |mut accum, spectrum| {
+                if spectrum.ms_level == 1 {
+                    accum.ms1.push(spectrum);
+                } else {
+                    accum.msn.push(spectrum);
+                }
+                accum
+            }).reduce(|| RawSpectrumAccumulator::default(), |mut a, b| {
+                a.ms1.extend(b.ms1);
+                a.msn.extend(b.msn);
+                a
+            });
+
+        out
+    }
 }
 
 impl Runner {
@@ -76,13 +107,14 @@ impl Runner {
     fn search_processed_spectra(
         &self,
         scorer: &Scorer,
-        spectra: Vec<ProcessedSpectrum>,
+        msn_spectra: Vec<ProcessedSpectrum<sage_core::spectrum::Peak>>,
+        ms1_spectra: MS1Spectra,
     ) -> SageResults {
         use std::sync::atomic::{AtomicUsize, Ordering};
         let counter = AtomicUsize::new(0);
         let start = Instant::now();
 
-        let features: Vec<_> = spectra
+        let features: Vec<_> = msn_spectra
             .par_iter()
             .filter(|spec| spec.peaks.len() >= self.parameters.min_peaks && spec.level == 2)
             .map(|x| {
@@ -113,16 +145,19 @@ impl Runner {
                 if level != 2 && level != 3 {
                     log::warn!("TMT quant level set at {}, is this correct?", level);
                 }
-                sage_core::tmt::quantify(&spectra, isobaric, Tolerance::Ppm(-20.0, 20.0), level)
+                sage_core::tmt::quantify(&msn_spectra, isobaric, Tolerance::Ppm(-20.0, 20.0), level)
             })
             .unwrap_or_default();
-        let ms1 = spectra.into_iter().filter(|s| s.level == 1).collect();
 
         SageResults {
             features,
             quant,
-            ms1,
+            ms1: ms1_spectra,
         }
+    }
+
+    fn requires_ms1(&self) -> bool {
+        self.parameters.quant.lfq
     }
 
     fn process_chunk(
@@ -161,7 +196,7 @@ impl Runner {
             min_deisotope_mz.unwrap_or(0.0),
         );
 
-        let spectra = chunk
+        let spectra: RawSpectrumAccumulator = chunk
             .par_iter()
             .enumerate()
             .flat_map(|(idx, path)| {
@@ -171,6 +206,7 @@ impl Runner {
                     file_id,
                     sn,
                     self.parameters.bruker_spectrum_processor,
+                    self.requires_ms1(),
                 );
 
                 match res {
@@ -183,14 +219,29 @@ impl Runner {
                         Err(e)
                     }
                 }
-            })
-            .flat_map_iter(|spectra| spectra.into_iter().map(|s| sp.process(s)))
+            }).flatten().collect();
+
+        let msn_spectra = spectra.msn.into_par_iter()
+            .map(|s| sp.process(s))
             .collect::<Vec<_>>();
+
+        // Note: Empty iterators return true.
+        let all_contain_ims = spectra.ms1.iter().all(|x| x.mobility.is_some());
+        let ms1_empty = spectra.ms1.is_empty();
+        let ms1_spectra = if ms1_empty {
+            MS1Spectra::Empty
+        } else if all_contain_ims {
+            let spectra = spectra.ms1.into_iter().map(|x| sp.process_with_mobility(x)).collect();
+            MS1Spectra::WithMobility(spectra)
+        } else {
+            let spectra = spectra.ms1.into_iter().map(|s| sp.process(s)).collect();
+            MS1Spectra::NoMobility(spectra)
+        };
 
         let io_time = Instant::now() - start;
         info!("- file IO: {:8} ms", io_time.as_millis());
 
-        self.search_processed_spectra(scorer, spectra)
+        self.search_processed_spectra(scorer, msn_spectra, ms1_spectra)
     }
 
     pub fn batch_files(&self, scorer: &Scorer, batch_size: usize) -> SageResults {

crates/sage/src/scoring.rs
@@ -2,7 +2,7 @@ use crate::database::{IndexedDatabase, PeptideIx};
 use crate::heap::bounded_min_heapify;
 use crate::ion_series::{IonSeries, Kind};
 use crate::mass::{Tolerance, NEUTRON, PROTON};
-use crate::spectrum::{Precursor, ProcessedSpectrum};
+use crate::spectrum::{Precursor, ProcessedSpectrum, Peak};
 use serde::{Deserialize, Serialize};
 use std::ops::AddAssign;
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -233,7 +233,7 @@ fn max_fragment_charge(max_fragment_charge: Option<u8>, precursor_charge: u8) ->
 }
 
 impl<'db> Scorer<'db> {
-    pub fn score(&self, query: &ProcessedSpectrum) -> Vec<Feature> {
+    pub fn score(&self, query: &ProcessedSpectrum<crate::spectrum::Peak>) -> Vec<Feature> {
         assert_eq!(
             query.level, 2,
             "internal bug, trying to score a non-MS2 scan!"
@@ -270,7 +270,7 @@ impl<'db> Scorer<'db> {
     /// in sorted order.
     fn matched_peaks_with_isotope(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<crate::spectrum::Peak>,
         precursor_mass: f32,
         precursor_charge: u8,
         precursor_tol: Tolerance,
@@ -318,7 +318,7 @@ impl<'db> Scorer<'db> {
 
     fn matched_peaks(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<Peak>,
         precursor_mass: f32,
         precursor_charge: u8,
         precursor_tol: Tolerance,
@@ -350,7 +350,7 @@ impl<'db> Scorer<'db> {
         }
     }
 
-    fn initial_hits(&self, query: &ProcessedSpectrum, precursor: &Precursor) -> InitialHits {
+    fn initial_hits(&self, query: &ProcessedSpectrum<Peak>, precursor: &Precursor) -> InitialHits {
         // Sage operates on masses without protons; [M] instead of [MH+]
         let mz = precursor.mz - PROTON;
 
@@ -399,7 +399,7 @@ impl<'db> Scorer<'db> {
     }
 
     /// Score a single [`ProcessedSpectrum`] against the database
-    pub fn score_standard(&self, query: &ProcessedSpectrum) -> Vec<Feature> {
+    pub fn score_standard(&self, query: &ProcessedSpectrum<Peak>) -> Vec<Feature> {
         let precursor = query.precursors.first().unwrap_or_else(|| {
             panic!("missing MS1 precursor for {}", query.id);
         });
@@ -414,7 +414,7 @@ impl<'db> Scorer<'db> {
     /// best PSMs ([`Feature`])
     fn build_features(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<Peak>,
         precursor: &Precursor,
         hits: &InitialHits,
         report_psms: usize,
@@ -527,7 +527,7 @@ impl<'db> Scorer<'db> {
     }
 
     /// Remove peaks matching a PSM from a query spectrum
-    fn remove_matched_peaks(&self, query: &mut ProcessedSpectrum, psm: &Feature) {
+    fn remove_matched_peaks(&self, query: &mut ProcessedSpectrum<Peak>, psm: &Feature) {
         let peptide = &self.db[psm.peptide_idx];
         let fragments = self
             .db
@@ -563,7 +563,7 @@ impl<'db> Scorer<'db> {
 
     /// Return multiple PSMs for each spectra - first is the best match, second PSM is the best match
     /// after all theoretical peaks assigned to the best match are removed, etc
-    pub fn score_chimera_fast(&self, query: &ProcessedSpectrum) -> Vec<Feature> {
+    pub fn score_chimera_fast(&self, query: &ProcessedSpectrum<Peak>) -> Vec<Feature> {
         let precursor = query.precursors.first().unwrap_or_else(|| {
             panic!("missing MS1 precursor for {}", query.id);
         });
@@ -592,7 +592,7 @@ impl<'db> Scorer<'db> {
     /// Calculate full hyperscore for a given PSM
     fn score_candidate(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<Peak>,
         pre_score: &PreScore,
     ) -> (Score, Option<Fragments>) {
         let mut score = Score {

lazear/sage

crates/sage-cli/src/runner.rs
@@ -14,7 +14,7 @@ use sage_core::mass::Tolerance;
 use sage_core::peptide::Peptide;
 use sage_core::scoring::Fragments;
 use sage_core::scoring::{Feature, Scorer};
-use sage_core::spectrum::{ProcessedSpectrum, SpectrumProcessor};
+use sage_core::spectrum::{MS1Spectra, ProcessedSpectrum, RawSpectrum, SpectrumProcessor};
 use sage_core::tmt::TmtQuant;
 use std::collections::HashMap;
 use std::collections::HashSet;
@@ -24,6 +24,43 @@ pub struct Runner {
     pub database: IndexedDatabase, // I could use a getter if I dont want to make this pub ...
     pub parameters: Search,
     start: Instant,
+}
+
+#[derive(Default)]
+struct RawSpectrumAccumulator {
+    pub ms1: Vec<RawSpectrum>,
+    pub msn: Vec<RawSpectrum>,
+}
+
+impl FromParallelIterator<RawSpectrum> for RawSpectrumAccumulator {
+    fn from_par_iter<I>(par_iter: I) -> Self
+    where
+        I: IntoParallelIterator<Item = RawSpectrum>,
+    {
+        let out = par_iter
+            .into_par_iter()
+            .fold(
+                || RawSpectrumAccumulator::default(),
+                |mut accum, spectrum| {
+                    if spectrum.ms_level == 1 {
+                        accum.ms1.push(spectrum);
+                    } else {
+                        accum.msn.push(spectrum);
+                    }
+                    accum
+                },
+            )
+            .reduce(
+                || RawSpectrumAccumulator::default(),
+                |mut a, b| {
+                    a.ms1.extend(b.ms1);
+                    a.msn.extend(b.msn);
+                    a
+                },
+            );
+
+        out
+    }
 }
 
 impl Runner {
@@ -82,11 +119,13 @@ impl Runner {
     }
 
     pub fn prefilter_peptides(self, parallel: usize, fasta: Fasta) -> Vec<Peptide> {
-        let spectra: Option<Vec<ProcessedSpectrum>> =
-            match parallel >= self.parameters.mzml_paths.len() {
-                true => Some(self.read_processed_spectra(&self.parameters.mzml_paths, 0, 0)),
-                false => None,
-            };
+        let spectra: Option<(
+            MS1Spectra,
+            Vec<ProcessedSpectrum<sage_core::spectrum::Peak>>,
+        )> = match parallel >= self.parameters.mzml_paths.len() {
+            true => Some(self.read_processed_spectra(&self.parameters.mzml_paths, 0, 0)),
+            false => None,
+        };
         let mut all_peptides: Vec<Peptide> = fasta
             .iter_chunks(self.parameters.database.prefilter_chunk_size)
             .enumerate()
@@ -112,13 +151,13 @@ impl Runner {
                     override_precursor_charge: self.parameters.override_precursor_charge,
                     max_fragment_charge: self.parameters.max_fragment_charge,
                     chimera: self.parameters.chimera,
-                    report_psms: self.parameters.report_psms + 1,
+                    report_psms: self.parameters.report_psms + 1, // Q: Why is 1 being added here? (JSPP: Feb 2024)
                     wide_window: self.parameters.wide_window,
                     annotate_matches: self.parameters.annotate_matches,
                     score_type: self.parameters.score_type,
                 };
                 let peptide_idxs: HashSet<PeptideIx> = match &spectra {
-                    Some(spectra) => self.peptide_filter_processed_spectra(&scorer, spectra),
+                    Some(spectra) => self.peptide_filter_processed_spectra(&scorer, &spectra.1),
                     None => self
                         .parameters
                         .mzml_paths
@@ -127,7 +166,7 @@ impl Runner {
                         .flat_map(|(chunk_idx, chunk)| {
                             let spectra_chunk =
                                 self.read_processed_spectra(chunk, chunk_idx, parallel);
-                            self.peptide_filter_processed_spectra(&scorer, &spectra_chunk)
+                            self.peptide_filter_processed_spectra(&scorer, &spectra_chunk.1)
                         })
                         .collect(),
                 }
@@ -152,7 +191,7 @@ impl Runner {
     fn peptide_filter_processed_spectra(
         &self,
         scorer: &Scorer,
-        spectra: &Vec<ProcessedSpectrum>,
+        spectra: &Vec<ProcessedSpectrum<sage_core::spectrum::Peak>>,
     ) -> Vec<PeptideIx> {
         use std::sync::atomic::{AtomicUsize, Ordering};
         let counter = AtomicUsize::new(0);
@@ -207,13 +246,13 @@ impl Runner {
     fn search_processed_spectra(
         &self,
         scorer: &Scorer,
-        spectra: &Vec<ProcessedSpectrum>,
+        msn_spectra: &Vec<ProcessedSpectrum<sage_core::spectrum::Peak>>,
     ) -> Vec<Feature> {
         use std::sync::atomic::{AtomicUsize, Ordering};
         let counter = AtomicUsize::new(0);
         let start = Instant::now();
 
-        let features: Vec<_> = spectra
+        let features: Vec<_> = msn_spectra
             .par_iter()
             .filter(|spec| spec.peaks.len() >= self.parameters.min_peaks && spec.level == 2)
             .map(|x| {
@@ -238,7 +277,8 @@ impl Runner {
 
     fn complete_features(
         &self,
-        spectra: Vec<ProcessedSpectrum>,
+        msn_spectra: Vec<ProcessedSpectrum<sage_core::spectrum::Peak>>,
+        ms1_spectra: MS1Spectra,
         features: Vec<Feature>,
     ) -> SageResults {
         let quant = self
@@ -251,16 +291,19 @@ impl Runner {
                 if level != 2 && level != 3 {
                     log::warn!("TMT quant level set at {}, is this correct?", level);
                 }
-                sage_core::tmt::quantify(&spectra, isobaric, Tolerance::Ppm(-20.0, 20.0), level)
+                sage_core::tmt::quantify(&msn_spectra, isobaric, Tolerance::Ppm(-20.0, 20.0), level)
             })
             .unwrap_or_default();
-        let ms1 = spectra.into_iter().filter(|s| s.level == 1).collect();
 
         SageResults {
             features,
             quant,
-            ms1,
+            ms1: ms1_spectra,
         }
+    }
+
+    fn requires_ms1(&self) -> bool {
+        self.parameters.quant.lfq
     }
 
     fn process_chunk(
@@ -271,8 +314,8 @@ impl Runner {
         batch_size: usize,
     ) -> SageResults {
         let spectra = self.read_processed_spectra(chunk, chunk_idx, batch_size);
-        let features = self.search_processed_spectra(scorer, &spectra);
-        self.complete_features(spectra, features)
+        let features = self.search_processed_spectra(scorer, &spectra.1);
+        self.complete_features(spectra.1, spectra.0, features)
     }
 
     fn read_processed_spectra(
@@ -280,7 +323,10 @@ impl Runner {
         chunk: &[String],
         chunk_idx: usize,
         batch_size: usize,
-    ) -> Vec<ProcessedSpectrum> {
+    ) -> (
+        MS1Spectra,
+        Vec<ProcessedSpectrum<sage_core::spectrum::Peak>>,
+    ) {
         // Read all of the spectra at once - this can help prevent memory over-consumption issues
         info!(
             "processing files {} .. {} ",
@@ -310,7 +356,7 @@ impl Runner {
             min_deisotope_mz.unwrap_or(0.0),
         );
 
-        let spectra = chunk
+        let spectra: RawSpectrumAccumulator = chunk
             .par_iter()
             .enumerate()
             .flat_map(|(idx, path)| {
@@ -320,6 +366,7 @@ impl Runner {
                     file_id,
                     sn,
                     self.parameters.bruker_spectrum_processor,
+                    self.requires_ms1(),
                 );
 
                 match res {
@@ -333,13 +380,36 @@ impl Runner {
                     }
                 }
             })
-            .flat_map_iter(|spectra| spectra.into_iter().map(|s| sp.process(s)))
+            .flatten()
+            .collect();
+
+        let msn_spectra = spectra
+            .msn
+            .into_par_iter()
+            .map(|s| sp.process(s))
             .collect::<Vec<_>>();
+
+        // Note: Empty iterators return true.
+        let all_contain_ims = spectra.ms1.iter().all(|x| x.mobility.is_some());
+        let ms1_empty = spectra.ms1.is_empty();
+        let ms1_spectra = if ms1_empty {
+            MS1Spectra::Empty
+        } else if all_contain_ims {
+            let spectra = spectra
+                .ms1
+                .into_iter()
+                .map(|x| sp.process_with_mobility(x))
+                .collect();
+            MS1Spectra::WithMobility(spectra)
+        } else {
+            let spectra = spectra.ms1.into_iter().map(|s| sp.process(s)).collect();
+            MS1Spectra::NoMobility(spectra)
+        };
 
         let io_time = Instant::now() - start;
         info!("- file IO: {:8} ms", io_time.as_millis());
 
-        spectra
+        (ms1_spectra, msn_spectra)
     }
 
     pub fn batch_files(&self, scorer: &Scorer, batch_size: usize) -> SageResults {

crates/sage/src/scoring.rs
@@ -2,7 +2,7 @@ use crate::database::{IndexedDatabase, PeptideIx};
 use crate::heap::bounded_min_heapify;
 use crate::ion_series::{IonSeries, Kind};
 use crate::mass::{Tolerance, NEUTRON, PROTON};
-use crate::spectrum::{Precursor, ProcessedSpectrum};
+use crate::spectrum::{Peak, Precursor, ProcessedSpectrum};
 use serde::{Deserialize, Serialize};
 use std::ops::AddAssign;
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -245,7 +245,7 @@ fn max_fragment_charge(max_fragment_charge: Option<u8>, precursor_charge: u8) ->
 impl<'db> Scorer<'db> {
     pub fn quick_score(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<Peak>,
         prefilter_low_memory: bool,
     ) -> Vec<PeptideIx> {
         assert_eq!(
@@ -284,7 +284,7 @@ impl<'db> Scorer<'db> {
         }
     }
 
-    pub fn score(&self, query: &ProcessedSpectrum) -> Vec<Feature> {
+    pub fn score(&self, query: &ProcessedSpectrum<crate::spectrum::Peak>) -> Vec<Feature> {
         assert_eq!(
             query.level, 2,
             "internal bug, trying to score a non-MS2 scan!"
@@ -321,7 +321,7 @@ impl<'db> Scorer<'db> {
     /// in sorted order.
     fn matched_peaks_with_isotope(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<crate::spectrum::Peak>,
         precursor_mass: f32,
         precursor_charge: u8,
         precursor_tol: Tolerance,
@@ -369,7 +369,7 @@ impl<'db> Scorer<'db> {
 
     fn matched_peaks(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<Peak>,
         precursor_mass: f32,
         precursor_charge: u8,
         precursor_tol: Tolerance,
@@ -401,7 +401,7 @@ impl<'db> Scorer<'db> {
         }
     }
 
-    fn initial_hits(&self, query: &ProcessedSpectrum, precursor: &Precursor) -> InitialHits {
+    fn initial_hits(&self, query: &ProcessedSpectrum<Peak>, precursor: &Precursor) -> InitialHits {
         // Sage operates on masses without protons; [M] instead of [MH+]
         let mz = precursor.mz - PROTON;
 
@@ -448,7 +448,7 @@ impl<'db> Scorer<'db> {
     }
 
     /// Score a single [`ProcessedSpectrum`] against the database
-    pub fn score_standard(&self, query: &ProcessedSpectrum) -> Vec<Feature> {
+    pub fn score_standard(&self, query: &ProcessedSpectrum<Peak>) -> Vec<Feature> {
         let precursor = query.precursors.first().unwrap_or_else(|| {
             panic!("missing MS1 precursor for {}", query.id);
         });
@@ -463,7 +463,7 @@ impl<'db> Scorer<'db> {
     /// best PSMs ([`Feature`])
     fn build_features(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<Peak>,
         precursor: &Precursor,
         hits: &InitialHits,
         report_psms: usize,
@@ -576,7 +576,7 @@ impl<'db> Scorer<'db> {
     }
 
     /// Remove peaks matching a PSM from a query spectrum
-    fn remove_matched_peaks(&self, query: &mut ProcessedSpectrum, psm: &Feature) {
+    fn remove_matched_peaks(&self, query: &mut ProcessedSpectrum<Peak>, psm: &Feature) {
         let peptide = &self.db[psm.peptide_idx];
         let fragments = self
             .db
@@ -612,7 +612,7 @@ impl<'db> Scorer<'db> {
 
     /// Return multiple PSMs for each spectra - first is the best match, second PSM is the best match
     /// after all theoretical peaks assigned to the best match are removed, etc
-    pub fn score_chimera_fast(&self, query: &ProcessedSpectrum) -> Vec<Feature> {
+    pub fn score_chimera_fast(&self, query: &ProcessedSpectrum<Peak>) -> Vec<Feature> {
         let precursor = query.precursors.first().unwrap_or_else(|| {
             panic!("missing MS1 precursor for {}", query.id);
         });
@@ -641,7 +641,7 @@ impl<'db> Scorer<'db> {
     /// Calculate full hyperscore for a given PSM
     fn score_candidate(
         &self,
-        query: &ProcessedSpectrum,
+        query: &ProcessedSpectrum<Peak>,
         pre_score: &PreScore,
     ) -> (Score, Option<Fragments>) {
         let mut score = Score {